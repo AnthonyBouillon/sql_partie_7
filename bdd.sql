c45c3d6a1929ee5841209b7d20927e3e7f351941
Anthony <anthony_bouillon@outlook.fr>
2018-01-15 12:22:40 +0100

    Suppression et modification de données

---
+++ bdd.sql
@@ -0,0 +1,17 @@
+SQL - Partie 7 : Suppression et modification de données
+
+Exercice 1
+Dans la table languages, supprimer toutes les lignes parlant de HTML.
+/* Je supprime dans la table languages la chaine de caractère 'HTML' dans la colonne language  */
+Ligne de commande : DELETE SELECT * FROM languages WHERE language = 'HTML';
+
+Exercice 2
+Dans la table frameworks, modifier toutes les lignes ayant le framework Symfony par Symfony2.
+/* Je modifie dans la table frameworks, la colonne framework et je modifie Symfony par Symfony2 */
+Ligne de commande : UPDATE frameworks SET framework = 'Symfony2' WHERE framework = 'Symfony';
+
+Exercice 3
+Dans la table languages, modifier la ligne du langage JavaScript version 5 par la version 5.1
+/* Je modifie dans la table languages, la colonne version qui ce trouve à la première ligne */
+Ligne de commande : UPDATE languages SET version = 5 WHERE id = 1;
+
